b1bbce017bd49a1019aafce3abbd4af17023f6ae
VarunHari12 <varunhari@outlook.com>
2024-09-30 21:53:04 -0700

    starter code

---
+++ main.py
@@ -0,0 +1,9 @@
+# step 1: import requests
+
+# step 2: call api with given endpoint and key
+
+# step 3: convert to dictionary and grab only the "data" value
+
+# step 4: loop through the flights and check which flights are active
+
+# step 5: if active, print the airline, date, departure airport and arrival airport
